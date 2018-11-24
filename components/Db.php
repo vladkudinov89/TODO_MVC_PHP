7edfe74b420ac27acb4a72ea3894e9b3c74c77de
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-11-24 09:43:16 +0300

    Add envdot package. Add .env file. Add README.md

--- components/Db.php
+++ components/Db.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace Components;
 
 class Db
 {
     public static function getConnection()
     {
-        $params = DbConnect::getConnection();
+        try {
+            $params = DbConnect::getConnection();
 
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new \PDO($dsn, $params['user'], $params['password']);
-        $db->exec("set names utf8");
-        return $db;
+            $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+            $db = new \PDO($dsn, $params['user'], $params['password']);
+            $db->exec("set names utf8");
+            return $db;
+        } catch (\PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
     }
 }
