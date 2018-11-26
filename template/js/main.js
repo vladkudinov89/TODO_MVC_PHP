75f193c99072ac7ce80ca135721a2071cc67de46
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-11-26 17:12:31 +0300

    Add preview task.

--- template/js/main.js
+++ template/js/main.js
@@ -33,21 +33,43 @@ $(document).ready(function () {
     });
 
     function getLength() {
-        if ($("th").length >= 7)
-        {
+        if ($("th").length >= 7) {
             return 6;
         }
     }
 
     $('#task-table').DataTable({
-        "info":     false,
+        "info": false,
         "lengthMenu": [[4, 10, 25, -1], [4, 10, 25, "All"]],
         stateSave: true,
         "columnDefs": [
             {
-                orderable: false, targets: [4,5 , getLength()]
+                orderable: false, targets: [4, 5, getLength()]
             }
         ]
+    });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#task_img').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('#preview_task_preview').click(function (e) {
+        e.preventDefault();
+
+        var task_name = $('#taskname').val();
+        var task_text = $('#tasktext').val();
+
+        $('#task_name').html(task_name);
+        $('#task_text').html(task_text);
+        readURL($('#taskphoto')[0]);
+
     });
 
 });
