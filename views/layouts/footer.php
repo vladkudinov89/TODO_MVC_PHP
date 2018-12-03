8b276499e0a24f87288f1be647cf5ed16003144c
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-12-03 20:58:57 +0300

    Change view footer.

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,9 +1,9 @@
-<footer id="footer"><!--Footer-->
-    <div class="footer-bottom">
-        <p class="pull-left">Copyright © 2018</p>
-        <p class="pull-right">MVC PHP</p>
-    </div>
-</footer><!--/Footer-->
+<footer id="footer" class="footer">
+        <div class="footer-bottom">
+            <p class="pull-left">Copyright © 2018</p>
+            <p class="pull-right">MVC PHP</p>
+        </div>
+</footer>
 
 </div>
 
