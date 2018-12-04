9accd88eb1aea16b5e5feddb20efd08bf49c2d19
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-12-04 14:48:19 +0300

    Resize big photo. Add interface to photoService.

--- services/PhotoService.php
+++ services/PhotoService.php
@@ -1,18 +1,39 @@
 <?php
 namespace Services;
 
-class PhotoService
+class PhotoService implements PhotoServiceInterface
 {
+    public const WIDTH = 320;
+    public const HEIGHT = 240;
+
     public static function addImage($image): string
     {
         if (isset($image)) {
 
             $imageType = $image['type'];
 
+            list($width, $height) = getimagesize($image['tmp_name']);
+//            print_r($w);
+//            print_r($h);
+
             if ($imageType == 'image/jpg' || $imageType == 'image/jpeg' || $imageType == 'image/png')
             {
-                $saveto = 'upload/' . basename($image["name"]);
-                move_uploaded_file($image['tmp_name'], $saveto);
+                if($width >= self::WIDTH || $height >= self::HEIGHT )
+                {
+                    $saveto = 'uploads/'.$width.'x'.$height.'_'.$image['name'];
+                    $image_p = imagecreatetruecolor(self::WIDTH, self::HEIGHT);
+//                    print_r("Big photo");
+                    imagecopyresampled(
+                        $image_p,
+                        $image,
+                        0, 0, 0, 0,
+                        self::WIDTH, self::HEIGHT,
+                        $width, $height);
+                } else {
+                    $saveto = 'upload/' . basename($image["name"]);
+                    move_uploaded_file($image['tmp_name'], $saveto);
+                }
+
                 return $saveto;
             }
         }
