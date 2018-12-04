4f8a4e106f3cf3b526789245fd6ff7af4e8ad1a1
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-12-04 11:07:52 +0300

    Add package PHPUnit. Make test work.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Models\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function testIsAdmin()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+    }
+
+//    public function testRegister()
+//    {
+//
+//    }
+//
+//    public function testCheckUserData()
+//    {
+//
+//    }
+}
