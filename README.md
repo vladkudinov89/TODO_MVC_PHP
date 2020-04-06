c4f4c8f829e3788a063225d7209d199cf8ef8c57
vladkudino89 <vlad.kudinov.89@gmail.com>
2020-04-06 13:46:15 +0300

    Fix docker problem. fix phinx. fix readme.

--- README.md
+++ README.md
@@ -14,11 +14,22 @@ git clone <this_repo>
 cp .env.example .env
 docker-compose up -d
 docker-compose exec php-cli composer install
+docker-compose exec php-fpm php vendor/bin/phinx migrate
 docker exec app-frontend yarn install
 docker exec app-frontend yarn run watch
+docker-compose exec php-fpm php vendor/bin/phinx seed:run
 ```
 ## Usage
 
 To start your containers you have only type next command:
 ```
 make docker-up
+```
+Before login in project. Route `http://localhost:8080/user/login`:
+`login`: admin@email.com
+`password`: password
+
+To stop your containers you have only type next command:
+```
+make docker-down
+```
---
+++ db/seeds/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'username'    => 'admin',
+                'email'    => 'admin@email.com',
+                'password'    => 'password',
+                'is_admin'    => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $users = $this->table('users');
+        $users->insert($data)
+            ->save();
+    }
+}
